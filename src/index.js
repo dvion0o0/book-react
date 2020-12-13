da074ffcf430bc3938a8546a4f3188e181e99fe2
Dv <dv@Dvs-MacBook-Pro.local>
2020-12-13 23:17:50 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -17,38 +17,51 @@ const books = [
     img:"https://m.media-amazon.com/images/I/81s6DUyQCZL._AC_UY436_QL65_.jpg",
     title:"Think Like a Monk",
     author:"Jay Shetty"
-  }
+  },  
+{
+  id:3,
+  img:"https://m.media-amazon.com/images/I/91mJmoqx5zL._AC_UY436_QL65_.jpg",
+  title:"Think and Grow Rich",
+  author:"Napoleon Hill"
+},
+{
+  id:4,
+  img:"https://m.media-amazon.com/images/I/81l3rZK4lnL._AC_UY436_QL65_.jpg",
+  title:"Ikigai: The Japanese Secret to a long and happy life",
+  author:"Hector Garcia"
+},
+{
+  id:5,
+  img:"https://m.media-amazon.com/images/I/71fsQxIlggL._AC_UL640_QL65_.jpg",
+  title:"Sapiens: A bBief History of HumanKind",
+  author:"Yuval Noah Harrari"
+},
+{
+  id:6,
+  img:"https://m.media-amazon.com/images/I/710jnzKlDTL._AC_UY436_QL65_.jpg",
+  title:"Attitude Is EveryThing",
+  author:"Keller Jeff"
+}
 ];
-
-const img = "https://m.media-amazon.com/images/I/81s6DUyQCZL._AC_UY436_QL65_.jpg";
 
 function BookList(){
   return <section className="booklist">
-<Book 
-img = {books[0].img} 
-title = {books[0].title}
-author = {books[0].author}
-/>
-<Book 
-img = {books[1].img} 
-title = {books[1].title}
-author = {books[1].author}
-/>
-<Book 
-img = {books[0].img} 
-title = {books[0].title}
-author = {books[0].author}
-/>
+    {books.map((book) =>{
+      return (
+      <Book key={book.id} {...book} />
+      )
+    })}
 
   </section>
 }
 
 
 function Book(props){
+  const{img, title, author} = props;
   return <article className="book">
-    <img src = {props.img} />
-    <h2>{props.title}</h2>
-    <h3>{props.author}</h3>
+    <img src = {img} alt="img" />
+    <h2>{title}</h2>
+    <h3>{author}</h3>
   </article>
 }
 
