9cc60bc729e21026c1be40a8f3e052bf3f03df80
Dv <dv@Dvs-MacBook-Pro.local>
2020-12-13 20:05:36 +0530

    added Css

--- src/index.js
+++ src/index.js
@@ -1,17 +1,56 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import "./index.css";
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+
+const books = [
+  {
+    id:1,
+    img:"https://m.media-amazon.com/images/I/9138VXjBfPL._AC_UY436_QL65_.jpg",
+    title:"The Power of Your Subconsious Mind",
+    author:"Dr. Joseph Murphy"
+  },
+  {
+    id:2,
+    img:"https://m.media-amazon.com/images/I/81s6DUyQCZL._AC_UY436_QL65_.jpg",
+    title:"Think Like a Monk",
+    author:"Jay Shetty"
+  }
+];
+
+const img = "https://m.media-amazon.com/images/I/81s6DUyQCZL._AC_UY436_QL65_.jpg";
+
+function BookList(){
+  return <section className="booklist">
+<Book 
+img = {books[0].img} 
+title = {books[0].title}
+author = {books[0].author}
+/>
+<Book 
+img = {books[1].img} 
+title = {books[1].title}
+author = {books[1].author}
+/>
+<Book 
+img = {books[0].img} 
+title = {books[0].title}
+author = {books[0].author}
+/>
+
+  </section>
+}
+
+
+function Book(props){
+  return <article className="book">
+    <img src = {props.img} />
+    <h2>{props.title}</h2>
+    <h3>{props.author}</h3>
+  </article>
+}
+
+
+ReactDOM.render(<BookList />, document.getElementById("root"));
